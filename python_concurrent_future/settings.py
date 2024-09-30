90b475471fa874eb2cfd656f20c8a7adc67d9e61
José Sobral <jose.sobral95@gmail.com>
2024-09-30 16:43:57 +0000

    Add python concurrent view and settings

--- python_concurrent_future/settings.py
+++ python_concurrent_future/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-ALLOWED_HOSTS = ['*']
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(tn00)suokilvl5-c8t*ma6&8@09taqpsgv!f11*cim+f5-fa$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'python_concurrent', 
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'python_concurrent_future.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'python_concurrent/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- python_concurrent_future/urls.py
+++ python_concurrent_future/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from python_concurrent import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('synchronous/', views.synchronous_view, name='synchronous'),
+    path('concurrent/', views.concurrent_view, name='concurrent'),
 ]
